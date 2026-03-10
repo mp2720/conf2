7eaf43e02e997470cceac8aaaff25cfbab6f2d8b
Salomakhin Timur <mp2720@proton.me>
2026-03-10 23:42:39 +0300

    add setup.sh
    Signed-off-by: Salomakhin Timur <mp2720@proton.me>

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+CONFIG_DIRS='alacritty fish helix i3'
+
+pp() {
+  echo "$@"
+  $@
+}
+
+for cfg_dir in $CONFIG_DIRS; do
+  for cfg_file in $(find "$cfg_dir" -type f); do
+    pp mkdir -p "$(dirname "$HOME/.config/$cfg_file")"
+    pp ln -sf "$(realpath "$cfg_file")" "$HOME/.config/$cfg_file"
+  done
+done
