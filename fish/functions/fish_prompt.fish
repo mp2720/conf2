6a2f155765d340b22e704a0679c1a38b986f23c3
Salomakhin Timur <mp2720@proton.me>
2026-06-11 16:50:16 +0300

    fish: fg jobs count in prompt
    Signed-off-by: Salomakhin Timur <mp2720@proton.me>

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -51,6 +51,13 @@ function fish_prompt --description 'Write out the prompt'
 
     printf '%s ' (fish_vcs_prompt)
 
+    set -l job_count (jobs | count)
+    if test $job_count -gt 0
+        set_color --bold yellow
+        echo -n "$job_count "
+        set_color normal
+    end
+
     set -l status_color (set_color $fish_color_status)
     set -l statusb_color (set_color --bold $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
