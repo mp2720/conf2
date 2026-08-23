d8e1a13dd45e0e70f316339305f4f15475fe9f12
Salomakhin Timur <mp2720@proton.me>
2026-08-23 06:42:41 +0300

    setup fonts using fontconfig
    Signed-off-by: Salomakhin Timur <mp2720@proton.me>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CONFIG_DIRS='alacritty fish helix i3 polybar myscripts xdg-desktop-portal'
+CONFIG_DIRS='alacritty fish helix i3 polybar myscripts xdg-desktop-portal fontconfig'
 
 pp() {
   echo "$@"
